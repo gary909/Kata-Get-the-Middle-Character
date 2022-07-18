2be8e5bb140a52001f08383cd3ab12e36bfa127d
gary <gary.white@lodgit.com>
2022-07-18 13:05:25 +0200

    code

---
+++ app.js
@@ -0,0 +1,8 @@
+function getMiddle(s) {
+    //Code goes here!
+}
+
+console.log(getMiddle("test")); // "es"
+console.log(getMiddle("testing")); // "t"
+console.log(getMiddle("middle")); // "dd"
+console.log(getMiddle("A")); // "A"
