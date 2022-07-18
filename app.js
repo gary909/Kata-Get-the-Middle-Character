8e493790b2729490f255b66b6496904fae332595
gary <gary.white@lodgit.com>
2022-07-18 13:41:45 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 function getMiddle(s) {
     //Code goes here!
+    let myArr = s.split('');
+    let myNum = s.length;
+    let myAns = Math.ceil(myNum / 2);
+    let myAns2 = Math.floor(myNum / 2);
+    if (s.length == 1) {
+        return s;
+    }
+    else if (myNum % 2 == 1) {
+        return myArr[myAns2];
+    } else if (myNum % 1 == 0) {
+        return myArr[s.length / 2 - 1] + myArr[myAns]
+    }
+
 }
 
 console.log(getMiddle("test")); // "es"
